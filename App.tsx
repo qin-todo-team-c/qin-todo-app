a52d1728e66ed9ba40d65982b0026b1ebe5a4bcf
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-02-20 10:07:17 +0900

    add: global style

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {box-sizing: border-box;}
+  /* other styles */
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <GlobalStyle />
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
