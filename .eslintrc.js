736e80a247c2451fafd5004d13691c91d52f91dd
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-02-20 17:20:17 +0900

    add: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,66 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    jest: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'import'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        extendDefaults: true,
+        types: {
+          '{}': false,
+        },
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.stories.tsx', 'src/pages/**/*'],
+      rules: {
+        'import/no-default-export': 'off',
+      },
+    },
+    {
+      files: ['*.test.ts', '*.mock.ts'],
+      rules: {
+        'max-lines': 'off',
+      },
+    },
+  ],
+};
