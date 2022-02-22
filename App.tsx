3b0a8691197d0cc1d28457f74e194b38d2876944
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-02-23 06:58:19 +0900

    fix: remove components do not follow the react native rules

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { createGlobalStyle } from 'styled-components';
-import { Avatar } from './src/components/atoms/Avatar';
-
-const GlobalStyle = createGlobalStyle`
-  * {box-sizing: border-box;}
-  /* other styles */
-`;
 
 export default function App() {
   return (
     <>
-      <GlobalStyle />
       <View style={styles.container}>
         <Text>Open up App.tsx to start working on your app!</Text>
-        <Avatar />
         <StatusBar style="auto" />
       </View>
     </>
