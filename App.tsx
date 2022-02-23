b9f388509d625fed234b7829389c20e4e6b29550
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-02-23 20:05:49 +0900

    fix: App.tsx import

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Avatar } from './src/components/atoms/Avatar';
-import { Logo } from './src/components/atoms/Logo';
 import { Header } from './src/components/organisms/Header';
 
 export default function App() {
